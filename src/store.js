d9bac54d8b07d590c545712ef0edf7f890eb4be9
Devendra-1997 <devendradhakal92@gmail.com>
2024-10-08 18:13:46 +1100

    updated store with transaction

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./redux/user/userSlice";
+import userReducer from "./redux/user/userSlice.js";
+import transactionReducer from "./redux/transaction/transactionSlice.js";
 const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: {
+    user: userReducer,
+    transaction: transactionReducer,
+  },
 });
 
 export default store;
